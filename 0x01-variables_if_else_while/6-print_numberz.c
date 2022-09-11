6439e77893f7852c9e3495e9cebe9710e52489ba
Manuel254 <immanuelkituku@gmail.com>
2022-09-10 23:30:03 -0700

    Add c file that prints digits using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always returns 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
