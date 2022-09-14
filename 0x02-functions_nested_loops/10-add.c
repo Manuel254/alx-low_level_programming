8a904f74b594a2ae206a7f66711353102a75b78a
Manuel254 <immanuelkituku@gmail.com>
2022-09-14 14:18:17 -0700

    Add c file that adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Adds two numbers.
+ * @a: The first integer operand
+ * @b: The second integer operand
+ *
+ * Return: Result of adding two numbers
+ */
+int add(int a, int b)
+{
+	int result;
+	
+	result = a + b;
+	return (result);
+}
+
