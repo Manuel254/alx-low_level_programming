9becb9d095eabd99bee77531b8182fc0f66542e5
Manuel254 <immanuelkituku@gmail.com>
2022-09-14 08:56:52 -0700

    Add c file that returns absolute of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute of a number
+ * @a: Number to be manipulated
+ *
+ * Return: Absolute number
+ */
+int _abs(int a)
+{
+	if (a < 0)
+		return (-a);
+	else
+		return (a);
+}
