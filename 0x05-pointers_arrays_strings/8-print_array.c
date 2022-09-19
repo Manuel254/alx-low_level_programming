656f254ccad57b58a40db3f1d9be3c03a11a7b06
Manuel254 <immanuelkituku@gmail.com>
2022-09-19 15:55:59 -0700

    Fix 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,19 +8,16 @@
 void print_array(int *a, int n)
 {
 	int i;
-	
-	if (n >= 0)
-	{	
-		for (i = 0; i < n; i++)
+
+	for (i = 0; i < n; i++)
+	{
+		if (a[i] != *(a+(n-1)))
 		{
-			if (a[i] != *(a+(n-1)))
-			{
-				printf("%d, ", a[i]);
-			}
-			else
-			{
-				printf("%d\n", a[i]);
-			}
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d\n", a[i]);
 		}
 	}
 }
