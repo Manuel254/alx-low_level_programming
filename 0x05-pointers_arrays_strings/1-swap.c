10e02882a7b9c113a2a3b914e7b4606602178b0c
Manuel254 <immanuelkituku@gmail.com>
2022-09-19 10:58:02 -0700

    Add c file that swaps values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+/**
+ * swap_int - swaps values of two variables
+ * @a: First variable
+ * @b: Second variable
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
