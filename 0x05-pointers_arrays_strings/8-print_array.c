906aca269710793d9a4bcccdf2f75eb3ad7149bc
Manuel254 <immanuelkituku@gmail.com>
2022-09-19 15:44:18 -0700

    Add c file that prints elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints elements of arrays
+ * @*a: pointer to integer array
+ * @n: number of elements of array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (a[i] != *(a+(n-1)))
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d\n", a[i]);
+		}
+	}
+}
+
