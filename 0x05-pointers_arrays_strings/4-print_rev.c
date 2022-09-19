4aec414475aca95872bc17b4a8a08f76c1391f45
Manuel254 <immanuelkituku@gmail.com>
2022-09-19 15:02:39 -0700

    Add c file that prints the reverse of a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: Address of first character of string
+ */
+void print_rev(char *s)
+{
+	char *temp;
+
+	temp = s;
+	while (*s != '\0')
+	{
+		++s;
+	}
+
+	while (temp <= s)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
