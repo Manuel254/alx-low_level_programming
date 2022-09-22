f4bbf5bae371e09ae49dbee2a55462501f361877
Manuel254 <immanuelkituku@gmail.com>
2022-09-22 00:52:12 -0700

    Add c file that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,34 @@
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: A signed integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int max, l1, l2, i, diff, result;
+
+	result = 0;
+	diff = 0;
+	l1 = strlen(s1);
+	l2 = strlen(s2);
+
+	if (l1 > l2)
+	{
+		max = l1;
+	}
+	else
+	{
+		max = l2;
+	}
+
+	for (i = 0; i < max; i++)
+	{
+		diff = (int) s1[i] - (int) s2[i];
+		result += diff;
+	}
+	return (result);
+}
