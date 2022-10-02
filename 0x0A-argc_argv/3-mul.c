f854db7da3cadfa31a2f861ae8e7c8cacd6ad7ef
Manuel254 <immanuelkituku@gmail.com>
2022-10-02 17:31:55 +0000

    Add c file that prints multiplication of two cmd line arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print result of multiplication
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if success else 1
+ */
+int main(int argc, char **argv)
+{
+	int result;
+
+	if (argc > 1)
+	{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+}
