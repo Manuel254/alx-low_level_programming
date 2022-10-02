89525f8a15171d01cc13068fb297f98a74149c61
Manuel254 <immanuelkituku@gmail.com>
2022-10-02 17:37:45 +0000

    Fix 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,8 +16,6 @@ int main(int argc, char **argv)
 	{
 		result = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", result);
-
-		return (0);
 	}
 	else
 	{
@@ -25,4 +23,5 @@ int main(int argc, char **argv)
 
 		return (1);
 	}
+	return (0);
 }
