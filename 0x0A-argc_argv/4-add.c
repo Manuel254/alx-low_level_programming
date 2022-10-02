a5e26c2f02e653b5c4c350e0f61c7e73542d176c
Manuel254 <immanuelkituku@gmail.com>
2022-10-02 18:28:19 +0000

    Add c file that adds numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints addition of numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if success else 1
+ */
+int main(int argc, char **argv)
+{
+	int i, sum;
+
+	sum = 0;
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("%d\n", 0);
+	}
+	return (0);
+}
