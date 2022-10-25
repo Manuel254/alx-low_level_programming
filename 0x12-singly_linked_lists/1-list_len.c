0c8d41096225422af10df096588fd6e62b2a2056
Manuel254 <immanuelkituku@gmail.com>
2022-10-24 17:10:29 -0700

    Add c file that gets length of a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - counts elements in a list
+ * @h: pointer to structure list_t
+ *
+ * Return: number of elements in a list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *ptr = h;
+
+	while (ptr != NULL)
+	{
+		count++;
+
+		ptr = ptr->next;
+	}
+	return (count);
+}
