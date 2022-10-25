0a663a9d615663562464063247af48e8a75b3d06
Manuel254 <immanuelkituku@gmail.com>
2022-10-24 17:03:40 -0700

    Add c file that prints list elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints elements of a list
+ * @h: pointer to structure list_t
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *ptr = h;
+
+	while (ptr != NULL)
+	{
+		count++;
+		if (ptr->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", ptr->len, ptr->str);
+	
+		ptr = ptr->next;
+	}
+	return (count);
+}
