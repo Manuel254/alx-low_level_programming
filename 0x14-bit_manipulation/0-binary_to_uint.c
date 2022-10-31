0a1b318ffaa51bfab7e89b42837fbd7a02c59404
Manuel254 <immanuelkituku@gmail.com>
2022-10-31 03:17:33 -0700

    Final soln

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * valid_bin - checks whether a string is valid binary
+ * @str: string to be checked
+ *
+ * Return: valid binary string or NULL
+ */
+unsigned int valid_bin(const char *str)
+{
+	int len, i;
+	len = strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		if (str[i] == '0' || str[i] == '1')
+			continue;
+		else
+			return (0);
+	}
+	return (atoi(str));
+}
+
+/**
+ * binary_to_uint - converts a binary to an unsigned int
+ * @b: string containing 0 and 1 characters
+ *
+ * Return: converted number or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal, weight, rem, bin;
+
+	decimal = 0;
+	weight = 1;
+	bin = valid_bin(b);
+
+	while (bin != 0)
+	{
+		rem = bin % 10;
+		decimal += rem * weight;
+		bin = bin / 10;
+		weight *= 2;
+	}
+	return (decimal);
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This is a directory containing all the tasks realted to the project bit manipulation
