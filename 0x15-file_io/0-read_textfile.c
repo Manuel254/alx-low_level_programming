2449b1e6947daab7f1eb6c2c570c440a719f829d
Manuel254 <immanuelkituku@gmail.com>
2022-11-03 06:42:58 -0700

    Fix 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,27 +12,32 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd, r, w, buff_count;
 	char *buffer;
 
-	buffer = malloc(sizeof(*buffer) * letters);
-
-	if (buffer == NULL || filename == NULL)
+	/* Check if filename is NULL */
+	if (filename == NULL)
 		return (0);
 
+	/* Open file */
 	fd = open(filename, O_RDONLY, 400);
-
 	if (fd == -1)
 		return (0);
 
-	r = read(fd, buffer, letters);
+	/* Create buffer */
+	buff_count = sizeof(*buffer) * letters;
+	buffer = malloc(buff_count);
+	if (buffer == NULL)
+		return (0);
 
+	/* Read file */
+	r = read(fd, buffer, letters);
 	if (r == -1)
 		return (0);
 
-	buff_count = sizeof(*buffer) * letters;
+	/* Write to output */
 	w = write(STDOUT_FILENO, buffer, buff_count);
-
 	if (w == -1 || w != buff_count)
 		return (0);
 
 	close(fd);
+	free(buffer);
 	return (r);
 }
