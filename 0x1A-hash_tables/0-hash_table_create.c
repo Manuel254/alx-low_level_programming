15c2b9b4a24f27d2e4fd3e5095a6c351413b48d3
Manuel254 <immanuelkituku@gmail.com>
2023-02-05 03:19:43 -0800

    Completed

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
-	if (hash_table == NULL)
+	if (hash_table == NULL || !size)
 		return (NULL);
 
 	hash_table->size = size;
