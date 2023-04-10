8ce94b7d14432322085f0b07935d2f150e2c6ab9
Manuel254 <immanuelkituku@gmail.com>
2023-04-10 10:19:01 -0700

    Free linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,9 +10,9 @@ void free_listint2(listint_t **head)
 
 	while (*head != NULL)
 	{
-		current = *head;
+		temp = *head;
 		*head = (*head)->next;
-		free(current)
+		free(temp);
 	}
 
 	*head = NULL;
