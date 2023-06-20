fcac8694cccf1a33752f141b19f6059de7b77f4d
ManuelTechs <immanuelkituku@gmail.com>
2023-06-20 06:12:05 -0700

    Add file that deletes a node from a linked list

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index of a linked list
+ * @head: pointer to head node
+ * @index: index of node
+ *
+ * Return: 1 if success else -1
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *prev = *head;
+	listint_t *curr = *head;
+
+	if (*head == NULL)
+		return (-1);
+	else if (index == 0)
+	{
+		*head = curr->next;
+		free(curr);
+		curr = NULL;
+	}
+	else
+	{
+		while (index != 0)
+		{
+			prev = curr;
+			curr = curr->next;
+			index--;
+		}
+		prev->next = curr->next;
+		free(curr);
+		curr = NULL;
+	}
+	return (1);
+}
